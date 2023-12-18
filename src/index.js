c62570e6df928a61e6f9bfb59fbe953a91f2900b
almagamdias <dias.almagambetov01@gmail.com>
2023-12-18 14:26:07 +0600

    REACT 1

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const text = 'GOOOOOOOOL';
+const elem = (
+  <div>
+    <h2>HELLO WORLD!!!</h2>
+    <p>{text}</p>
+    <span>NOW IS {['324234']}</span>
+    <input type="text" />
+    <button>Click</button>
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  elem
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
